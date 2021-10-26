4d385d491d53f4a209286ac7f8b5188ddb6be8b9
Jack Schollmeyer <jack@schollmeyer.com>
2021-10-26 16:38:20 -0500

    progress made on home page, added contact page with link button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import Link from "next/link";
 import s from "../../styles/Navbar.module.css";
 
 export default function Navbar({ children }) {
+  const [showSidebar, setShowSidebar] = useState(0);
+
   return (
     <div>
       <nav className={s.navbar}>
         <h1 className={s.navbarHeader}>Destinations</h1>
         <ul className={s.navbarLinks}>
-          <li>HOME</li>
-          <li>EXPLORE</li>
-          <li>CONTACT</li>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>Explore</li>
+          <li>
+            <Link href="/contact">
+              <a>Contact</a>
+            </Link>
+          </li>
         </ul>
       </nav>
       <main className={s.main}>{children}</main>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,47 @@
 import Image from "next/image";
-import Background from "../public/lakeBetweenMountains.jpg";
+import Background from "../public/lightInSnow.jpg";
 import s from "../../styles/Home.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faCodepen,
+  faFreeCodeCamp,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Home() {
   return (
     <div>
+      <div className={s.headerContainer}>
+        <h1 className={s.header}>discover the world</h1>
+      </div>
       <div className={s.img}>
         <Image src={Background} alt="Lake Between Mountains" />
       </div>
+      <footer className={s.footer}>
+        <h2>you can find me at</h2>
+        <div className={s.mediaIconsContainer}>
+          <div>
+            <FontAwesomeIcon icon={faGithub} className={s.githubIcon} />
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faCodepen} className={s.codepenIcon} />
+          </div>
+          <div>
+            <FontAwesomeIcon
+              icon={faFreeCodeCamp}
+              className={s.freeCodeCampIcon}
+            />
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faInstagram} className={s.instagramIcon} />
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faEnvelope} className={s.emailIcon} />
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
