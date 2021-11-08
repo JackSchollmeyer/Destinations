b57ffe367752d8e599c4dba1aff9afc9a9cc0718
Jack Schollmeyer <jack@schollmeyer.com>
2021-11-08 09:31:16 -0600

    Mostly finished top of home page, still need to add more info as you scroll down

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import s from "../../styles/HomepageStyles/Home.module.css";
 import FeaturedSection from "../components/HomepageComponents/FeaturedSection";
 import QuoteBoxCarousel from "../components/HomepageComponents/QuoteBoxCarousel";
 import Footer from "../components/HomepageComponents/Footer";
-import Background from "../public/rockyIsland.jpg";
+import Background from "../public/manLookingAtMountain.jpeg";
 
 export default function Home() {
   return (
@@ -14,16 +14,17 @@ export default function Home() {
             <h1 className={s.header}>
               Welcome to <span>Destinations</span>
             </h1>
-            <h2>Discover Gods amazing creation</h2>
+            <h2>- Discover the amazing world that God created -</h2>
           </div>
-          <div className={s.purposeContainer}>
+          {/* <div className={s.purposeContainer}>
             <h2>Purpose</h2>
             <p>
               My goal is to give others the chance to explore & learn about the
               fascinating landscapes in the world.
             </p>
-          </div>
+          </div> */}
         </div>
+        <div className={s.backgroundTriangle}></div>
         <div className={s.img}>
           <Image src={Background} alt="Rocky Island" />
         </div>
