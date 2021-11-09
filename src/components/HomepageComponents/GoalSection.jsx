363ae95af3ce2909e764dcd3040a4fdd08088c85
Jack Schollmeyer <jack@schollmeyer.com>
2021-11-09 10:20:07 -0600

    finished goal sectiion

--- src/components/HomepageComponents/GoalSection.jsx
+++ src/components/HomepageComponents/GoalSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import s from "../../../styles/HomepageStyles/GoalSection.module.css";
-import Discover from "../../public/discover.svg";
+import Reveal from "../../public/reveal.svg";
 import Inform from "../../public/inform.svg";
 import Inspire from "../../public/inspire.svg";
 
@@ -11,45 +11,39 @@ export default function GoalSection() {
       <h2 className={s.header}>What's the purpose?</h2>
       <div className={s.goalContainer}>
         <div className={s.goalsRight}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip
-          </p>
           <div>
-            <h3>To discover</h3>
-            <div className={s.illustrations}>
-              <Image src={Discover} alt="" />
-            </div>
+            <h3>To reveal</h3>
+            <p>
+              Bring to light new and astonishing locations that will blow you
+              away with their magnificence.
+            </p>
+          </div>
+          <div className={s.illustrations}>
+            <Image src={Reveal} alt="" />
           </div>
         </div>
         <div className={s.goalsLeft}>
+          <div className={s.illustrations}>
+            <Image src={Inform} alt="" />
+          </div>
           <div>
             <h3>To inform</h3>
-            <div className={s.illustrations}>
-              <Image src={Inform} alt="" />
-            </div>
+            <p>
+              Learn about the origins and history of these landmarks and what
+              makes them significant.
+            </p>
           </div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip
-          </p>
         </div>
         <div className={s.goalsRight}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip
-          </p>
           <div>
             <h3>To inspire</h3>
-            <div className={s.illustrations}>
-              <Image src={Inspire} alt="" />
-            </div>
+            <p>
+              Search for your own destinations that influence you and learn
+              about what makes them extraordinary.
+            </p>
+          </div>
+          <div className={s.illustrations}>
+            <Image src={Inspire} alt="" />
           </div>
         </div>
       </div>
