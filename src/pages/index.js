480ef52980ce5157e593b930bcf526783abf0c82
Jack Schollmeyer <jack@schollmeyer.com>
2021-11-19 18:44:14 -0600

    started redesigning and restructuring hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,18 +8,16 @@ import Background from "../public/manLookingAtMountain.jpeg";
 export default function Home() {
   return (
     <div>
-      <div>
-        <div className={s.homeContainer}>
-          <div className={s.headerContainer}>
-            <h1 className={s.header}>
-              Welcome to <span>Destinations</span>
-            </h1>
-            <h2>- Discover the amazing world that God created -</h2>
+      <div className={s.backgroundImg}>
+        <div className={s.backgroundOverlay}>
+          <div className={s.homeContainer}>
+            <div className={s.headerContainer}>
+              <h1 className={s.header}>
+                Welcome to <span>Destinations</span>
+              </h1>
+              <h2>- Discover the amazing world that God created -</h2>
+            </div>
           </div>
-        </div>
-        <div className={s.backgroundTriangle}></div>
-        <div className={s.img}>
-          <Image src={Background} alt="Rocky Island" />
         </div>
       </div>
       <GoalSection />
