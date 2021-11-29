5fa91493ceb7df34218011c0dbb2a6bcc26445e0
Jack Schollmeyer <jack@schollmeyer.com>
2021-11-29 09:27:05 -0600

    finished about me section

--- src/components/HomepageComponents/BioSection.jsx
+++ src/components/HomepageComponents/BioSection.jsx
@@ -19,9 +19,9 @@ export default function BioSection() {
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <Link href="/">
-            <a>Portfolio</a>
-          </Link>
+          <div className={s.button}>
+            <Link href="/">Portfolio</Link>
+          </div>
         </div>
         <div className={s.img}>
           <Image src={BioPicture} alt="ME" />
